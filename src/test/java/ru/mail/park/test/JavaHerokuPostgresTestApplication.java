ccbdf38cdeaa99ecde1b2b3ec707de75a15996da
Ivan Sopov <sopov.ivan@gmail.com>
2016-10-22 14:48:39 +0300

    fix for local test launch

--- src/test/java/ru/mail/park/test/JavaHerokuPostgresTestApplication.java
+++ src/test/java/ru/mail/park/test/JavaHerokuPostgresTestApplication.java
@@ -2,8 +2,12 @@ package ru.mail.park.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import ru.mail.park.JavaHerokuPostgresApplication;
 
 @SpringBootApplication
+@Import(JavaHerokuPostgresApplication.class)
 public class JavaHerokuPostgresTestApplication {
 
 	public static void main(String[] args) {
